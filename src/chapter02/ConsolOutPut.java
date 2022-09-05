560016a07d16481eabe3811dcd45e8b48a7ca0dd
Soyun Kim <soyun0661@gmail.com>
2022-09-05 17:50:18 +0900

    두번째 커밋 - 새로운 파일 생성, 기존의 파일을 수정 (추가, 삭제)

--- src/chapter02/ConsolOutPut.java
+++ src/chapter02/ConsolOutPut.java
@@ -13,6 +13,8 @@ public class ConsolOutPut {
 		
 		System.out.print("안녕 \n");  // \n : enter
 		System.out.print("하세요. ");
+
+                           System.out.println("추가된 내용1");
 	
  	}
 
--- src/chapter02/ConsolOutput02.java
+++ src/chapter02/ConsolOutput02.java
@@ -2,14 +2,7 @@ package chapter02;
 
 public class ConsolOutput02 {
      public static void main(String[] args) {
-		 //# printf   
-    	 //      %d     :   정수 값을 10진수로 불러들임
-    	 //      %o     :   정수 값을 8진수로 불러들임
-    	 //      %x     :   정수 값을 16진수로 불러들임
-    	 //      %s     :   문자열을 불러들임
-    	 //      %f     :   실수 값을 불러들임
-    	 //      %4.2f  :   전체 4 자리 소수점 이하 2자리 실수 불러들임
-    	 
+	
     	 System.out.printf("%d 살입니다. \n", 30);     //30 (10진수)
     	 System.out.printf("%o : 8진수로 출력 \n", 30);            //36  (8진수)
     	 System.out.printf("%x : 16진수로 출력 \n", 30);
